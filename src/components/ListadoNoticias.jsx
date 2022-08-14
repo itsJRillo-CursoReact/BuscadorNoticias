0435373a71ad43d6b7bb7eeb822d9154828dfbde
itsJRillo <jmorillolabour@gmail.com>
2022-08-14 20:24:36 +0200

    Proyecto Acabado

---
+++ src/components/ListadoNoticias.jsx
@@ -0,0 +1,49 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+import useNoticias from "../hooks/useNoticias";
+import Noticia from "./Noticia";
+
+const ListadoNoticias = () => {
+  const { noticias, handleChangePagina,totalNoticias, pagina} = useNoticias();
+  const totalPaginas = Math.ceil(totalNoticias / 20)
+
+  return (
+    <>
+      <Typography
+        textAlign={"center"}
+        marginY={5}
+        variant="h3"
+        component={"h2"}
+        sx={{
+            fontWeight:"bold"
+        }}
+      >
+        Últimas Noticias
+      </Typography>
+
+      <Grid container spacing={4}>
+        {noticias.map((noticia) => (
+          <Noticia noticia={noticia} key={noticia.url} />
+        ))}
+      </Grid>
+
+      <Stack 
+            sx={{marginY:5}}
+            spacing={2} 
+            direction={"row"}
+            justifyContent="center"
+            alignItems="center"
+
+      >
+        
+        <Pagination page={pagina} count={totalPaginas} color="primary" onChange={handleChangePagina}/>
+        
+      </Stack>
+    </>
+  );
+};
+
+export default ListadoNoticias;
